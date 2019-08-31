3528975607ff8946a15805b3a0462972a0c98aac
dingzb <dingzb@keruyun.com>
2019-08-31 15:34:25 +0800

    优惠券bug修复

--- marketing-data/src/main/java/com/zhongmei/yunfu/bean/req/CustomerCouponResp.java
+++ marketing-data/src/main/java/com/zhongmei/yunfu/bean/req/CustomerCouponResp.java
@@ -136,7 +136,7 @@ public class CustomerCouponResp implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((id == null) ? 0 : customerCouponId.hashCode());
         return result;
     }
 
